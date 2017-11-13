e862f58b3e2c01f475c046085e56f5b193431d83
Chris Luiz <chris@chrisluiz.io>
2017-11-13 07:30:06 -0500

    db & dockerfile updates

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,34 @@
+import datetime
+import os
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+
+# set config
+app_Settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
+
+# instantiate db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), defaul=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
 
 
 @app.route('/ping', methods=['GET'])
