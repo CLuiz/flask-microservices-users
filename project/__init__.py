a54ac9f24c51400af81ad0407c4ea8a9273ece45
Chris Luiz <chris@chrisluiz.io>
2017-11-14 20:49:46 -0500

    postgres setup complete

--- project/__init__.py
+++ project/__init__.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 
 
 # set config
-app_Settings = os.getenv('APP_SETTINGS')
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 # instantiate db
 db = SQLAlchemy(app)
 
 # model
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), defaul=False, nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, username, email):
