f6696b169591a88ce73b75a1625067a035c62855
Chris Luiz <chris@chrisluiz.io>
2017-11-17 22:29:47 -0500

    add user route and test

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -13,3 +13,18 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data = json.dumps(dict(
+                    username='chris',
+                    email='chris@chrisluiz.io'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('chris@chrisluiz.io was added!', data['message'])
+            self.asssertIn('success', data['status'])
